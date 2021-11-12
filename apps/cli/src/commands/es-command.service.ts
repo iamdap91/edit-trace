737030a4cf7fd9e6c4f57fbfaa52a45ba262890e
3sam3 <iamdap91@gmail.com>
2021-11-13 03:32:18 +0900

    [engine] feature/engine-clis es 인덱스 생성 cli 작서

--- apps/cli/src/commands/es-command.service.ts
+++ apps/cli/src/commands/es-command.service.ts
@@ -1,9 +1,24 @@
 import { Console, Command } from 'nestjs-console';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
 
 @Console({ name: 'elasticsearch', alias: 'es' })
 export class EsCommandService {
+  constructor(private elasticsearchService: ElasticsearchService) {}
+
   @Command({ command: 'create-index <name>' })
-  async createIndex() {
+  async createIndex(name: string) {
+    const res = await this.elasticsearchService.indices.create({
+      index: name,
+      body: {
+        settings: {
+          number_of_shards: 5,
+          number_of_replicas: process.env.MODE === 'prod' ? 1 : 0,
+        },
+        mappings: {},
+      },
+    });
+
+    console.log(res.body);
     console.log('인덱스 생성');
   }
 }
