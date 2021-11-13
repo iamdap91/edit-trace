d9e202b96b7fa41812704ed4a14d1c678b029359
sam3 <45986023+iamdap91@users.noreply.github.com>
2021-11-14 03:31:09 +0900

    [doc] swagger 추가 (#6)
    * [api] feature/swagger 스웨거 모듈 추가
    
    * [api] feature/swagger 스웨거 적용
    
    * [api] feature/swagger 메인 주석 추가
    
    * [api] feature/swagger api 스웨거 적용

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,18 +2,29 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { environment } from './environments/environment.prod';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3333;
+  const mode = environment.production ? 'production' : 'dev';
+  const url = `http://localhost:${port}`;
+  const apiPath = 'api-docs';
+  const apiDocUrl = `${url}/${apiPath}`;
+
+  // app 설정
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
 
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  // swagger
+  const config = new DocumentBuilder().setTitle('HIED').setDescription('하이드 API 문서').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(apiPath, app, document);
 
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log(`Listening at http://localhost:${port}/${globalPrefix}`);
-  });
+  // server init
+  await app.listen(process.env.PORT || 3333);
+  Logger.log(`Run server mode: ${mode}, Listening at ${url}/api, API doc is ${apiDocUrl}`);
 }
 
 bootstrap();
