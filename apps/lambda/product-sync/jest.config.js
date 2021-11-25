1b2ccd7376ec8e910497b501fcf80911bbde1a0f
3sam3 <iamdap91@gmail.com>
2021-11-25 23:37:43 +0900

    [engine] feature/lambda-product-sync 람다 크롤러 추가

---
+++ apps/lambda/product-sync/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'lambda-product-sync',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/lambda/product-sync',
+};
