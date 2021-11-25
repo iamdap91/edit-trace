9537994b5dd22a6a2bc08f5643663d745ca33770
3sam3 <iamdap91@gmail.com>
2021-11-25 17:33:34 +0900

    [engine] feature/format-serializer 메이시스 쿠키 추가

--- libs/engine/src/A025/A025.service.ts
+++ libs/engine/src/A025/A025.service.ts
@@ -2,6 +2,11 @@ import { Page } from 'puppeteer';
 
 export class A025Service {
   async product(url: string, page: Page) {
+    await page.setCookie({
+      name: 'currency',
+      value: 'USD',
+      url: 'https://www.macys.com',
+    });
     await page.goto(url, { waitUntil: 'domcontentloaded' });
     const bootstrap = await page.evaluate(() => window?.['__INITIAL_STATE__']?.['_PDP_BOOTSTRAP_DATA']);
     return JSON.parse(bootstrap)?.product;
