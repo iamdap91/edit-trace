36de40c6b0d459652c118780e60bccd9c225796b
3sam3 <iamdap91@gmail.com>
2021-11-25 19:04:19 +0900

    [engine] feature/format-serializer SyncedProductSerializer 추가

--- libs/engine/src/A025/A025.service.ts
+++ libs/engine/src/A025/A025.service.ts
@@ -1,4 +1,5 @@
 import { Page } from 'puppeteer';
+import { SyncedProductSerializer } from '@edit-trace/models';
 
 export class A025Service {
   async product(url: string, page: Page) {
@@ -9,6 +10,29 @@ export class A025Service {
     });
     await page.goto(url, { waitUntil: 'domcontentloaded' });
     const bootstrap = await page.evaluate(() => window?.['__INITIAL_STATE__']?.['_PDP_BOOTSTRAP_DATA']);
-    return JSON.parse(bootstrap)?.product;
+    const product = JSON.parse(bootstrap)?.product;
+
+    if (!product) throw new Error('상품 정보 로드 실패');
+
+    return new SyncedProductSerializer({
+      timestamp: new Date().toString(),
+      id: product.id,
+      name: product.detail?.name,
+      currency: 'USD',
+      images: product?.imagery?.images?.map((image) => `${product?.urlTemplate?.product}${image?.filePath}`) || [],
+      ...A025Service.parsePrice(product?.pricing),
+    });
+  }
+
+  private static parsePrice(pricing) {
+    const [priceInfo, salePriceInfo] = pricing.price.tieredPrice;
+
+    const price = priceInfo?.values?.[0]?.value;
+    const salePrice = salePriceInfo?.values?.[0]?.value;
+
+    return {
+      price,
+      salePrice: salePrice ?? price,
+    };
   }
 }
