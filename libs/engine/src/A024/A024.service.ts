3484fb18d6eaf5862021e036efa3576be59ec5da
3sam3 <iamdap91@gmail.com>
2021-11-25 19:54:22 +0900

    [engine] feature/format-serializer SyncedProductSerializer 상품 크롤링 리턴값 시리얼라이저 적용

--- libs/engine/src/A024/A024.service.ts
+++ libs/engine/src/A024/A024.service.ts
@@ -1,18 +1,26 @@
 import { Page } from 'puppeteer';
+import { SyncedProductSerializer } from '@edit-trace/models';
 
 export class A024Service {
   async product(url: string, page: Page) {
     const productId = new URL(url)?.searchParams.get('ID');
-
     await page.goto(url, { waitUntil: 'domcontentloaded' });
-    const response = await page.evaluate(() =>
-      fetch(`https://www.bloomingdales.com/xapi/digital/v1/product/${productId}`).then((res) => res?.json())
+    const response = await page.evaluate(
+      (id: string) => fetch(`https://www.bloomingdales.com/xapi/digital/v1/product/${id}`).then((res) => res?.json()),
+      productId
     );
 
     const product = response?.product?.[0];
     if (!product) throw new Error('상품 정보 로드 실패');
 
-    return product;
+    return new SyncedProductSerializer({
+      timestamp: new Date().toString(),
+      id: product.id,
+      name: product.detail?.name,
+      currency: 'USD',
+      images: product?.imagery?.images?.map((image) => `${product?.urlTemplate?.product}${image?.filePath}`) || [],
+      ...A024Service.parsePrice(product?.pricing),
+    });
   }
 
   // todo 나중에 기능 분리해서 다른데서 써먹자
@@ -29,5 +37,17 @@ export class A024Service {
       console.log(e);
     }
     return res;
+  }
+
+  private static parsePrice(pricing) {
+    const [priceInfo, salePriceInfo] = pricing.price.tieredPrice;
+
+    const price = priceInfo?.values?.[0]?.value;
+    const salePrice = salePriceInfo?.values?.[0]?.value;
+
+    return {
+      price,
+      salePrice: salePrice ?? price,
+    };
   }
 }
--- libs/engine/src/base-engine.ts
+++ libs/engine/src/base-engine.ts
@@ -1,8 +1,9 @@
-import { EngineInterface, FormattedProductInterface } from './interfaces';
+import { SyncedProductSerializer } from '@edit-trace/models';
+import { EngineInterface } from './interfaces';
 
 export class BaseEngine implements EngineInterface {
-  async product(targetUrl, browser): Promise<FormattedProductInterface>;
-  async product(): Promise<FormattedProductInterface> {
+  async product(targetUrl, browser): Promise<SyncedProductSerializer>;
+  async product(): Promise<SyncedProductSerializer> {
     throw new Error('product 기능 개발중');
   }
 }
